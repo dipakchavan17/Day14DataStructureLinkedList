12f66d51def3dc94bd1ad1ca86e9ec4df93ffba7
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-03 19:52:24 +0530

    deleted first element

--- LinkedList/LinkedList/LinkedList1.cs
+++ LinkedList/LinkedList/LinkedList1.cs
@@ -8,12 +8,14 @@ namespace LinkedList
 {
     internal class LinkedList1
     {
-        internal Node head; //new
+        internal Node head;
         internal void Add(int data)
         {
             Node node = new Node(data);
             if (this.head == null)
+            {
                 this.head = node;
+            }
             else
             {
                 Node temp = head;
@@ -23,69 +25,73 @@ namespace LinkedList
                 }
                 temp.next = node;
             }
-            Console.WriteLine("{0} inserted into the linked list", node.data);
-
+            Console.Write("\n {0} inserted into linked list", node.data);
         }
-        internal void AddFirst(int data)
+        public void AddinreverseOrder(int data)
         {
-            Node node = new Node(data);
+            Node newNode = new Node(data);
             if (this.head == null)
-                this.head = node;
-            else
             {
-                Node temp = head;
-                while (temp.next != null)
-                {
-                    temp = temp.next;
-                }
-                temp.next = node;
-            }
-            Console.WriteLine("{0} inserted into the Append linked list", node.data);
-        }
-        internal void Display()
-        {
-            Node temp = this.head;
-            if (temp == null)
-            {
-                Console.WriteLine("\nLinked list is empty");
-                return;
-            }
-            while (temp != null)
-            {
-                Console.Write(temp.data + " ");
-                temp = temp.next; //temp=null
-            }
-            Console.WriteLine("\n");
-        }
-        internal Node InsertAtParticularPosition(int position, int data)
-        {
-            if (position < 1)
-                Console.WriteLine("Invalid position");
-            if (position == 1)
-            {
-                var newNode = new Node(data);
-                newNode.next = this.head;
-                head = newNode;
+                this.head = newNode;
             }
             else
             {
                 Node temp = this.head;
-                while (position-- != 0) //
-                {
-
-                    if (position == 1)
-                    {
-                        Node node = new Node(data);
-                        node.next = this.head.next;
-                        head.next = node;
-                        break;
-                    }
-                    temp = temp.next;//1000
-                }
-                if (position != 1)
-                    Console.WriteLine("Position out of range");
+                head = newNode;
+                head.next = temp;
             }
-            return head;
+            Console.Write("\n {0} inserted into linked list", newNode.data);
+        }
+        internal void Display()
+        {
+            Console.Write("\n Data of linked list : ");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                return newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return this.head;
+            }
+            Node prev = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                prev = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = prev.next;
+            prev.next = newestNode;
+            return this.head;
+        }
+        internal Node RemoveFirstNode()
+        {
+            if (this.head == null)
+            {
+                return null;
+            }
+            this.head = this.head.next;
+            return this.head;
         }
     }
 }
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -11,21 +11,17 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List");
-            LinkedList1 list = new LinkedList1();
-            list.Add(56);
-            //list.Add(30);
-            list.Add(70);
-            list.InsertAtParticularPosition(2, 30);
-            LinkedList1 list2 = new LinkedList1();
-            //Appends the Nodes
-            list2.AddFirst(56);
-            list2.AddFirst(30);
-            list2.AddFirst(70);
-            list2.Display();
-            Console.ReadKey();
-            list.Display();
+
+            LinkedList1 linkedlist = new LinkedList1();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+            linkedlist.RemoveFirstNode();
+            linkedlist.Display();
             Console.ReadLine();
         }
+        
+        
 
       
         }
