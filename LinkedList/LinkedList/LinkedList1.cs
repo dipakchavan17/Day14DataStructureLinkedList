9d375e4ca88e56b43a2fb3482a67aaf6d25de3ff
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-03 19:47:49 +0530

    Inserted in between

--- LinkedList/LinkedList/LinkedList1.cs
+++ LinkedList/LinkedList/LinkedList1.cs
@@ -57,5 +57,35 @@ namespace LinkedList
             }
             Console.WriteLine("\n");
         }
+        internal Node InsertAtParticularPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid position");
+            if (position == 1)
+            {
+                var newNode = new Node(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                while (position-- != 0) //
+                {
+
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                    }
+                    temp = temp.next;//1000
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
     }
 }
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -13,9 +13,9 @@ namespace LinkedList
             Console.WriteLine("Welcome to Linked List");
             LinkedList1 list = new LinkedList1();
             list.Add(56);
-            list.Add(30);
+            //list.Add(30);
             list.Add(70);
-            //list.InsertAtParticularPosition(2,30);
+            list.InsertAtParticularPosition(2, 30);
             LinkedList1 list2 = new LinkedList1();
             //Appends the Nodes
             list2.AddFirst(56);
