bf1fcd465db76f66cb883b09ac9408f1dd569213
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-03 20:25:06 +0530

    ordered list

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -11,15 +11,46 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List");
+            LinkedList1 list2 = new LinkedList1();
+            //Appends the Nodes
+            list2.AddFirst(56);
+            list2.AddFirst(30);
+            list2.AddFirst(70);
 
-            LinkedList1 linkedlist = new LinkedList1();
-            linkedlist.Add(56);
-            linkedlist.Add(30);
-            linkedlist.Add(40);
-            linkedlist.Add(70);
-            linkedlist.Display();
-            int position = linkedlist.Search(40);
-            linkedlist.DeleteNodeAtParticularPosition(position);
+            //Remove first node 50 from the linked list
+            list2.RemoveFirstNode();
+            list2.Display();
+            //Removed Last Node 70 from the linked list
+            Console.WriteLine("Removed the last node from the linked list");
+            LinkedList1 list5 = new LinkedList1();
+            list5.Add(56);
+            list5.Add(30);
+            list5.Add(70);
+            list5.RemovaLastNode();
+            list5.Display();
+            //Search Node 30 from the given liked list
+            Console.WriteLine("Searching the Nodes");
+            LinkedList1 list6 = new LinkedList1();
+            list6.Add(40);
+            list6.Add(30);
+            list6.Add(70);
+            //remove Node 40
+
+            list6.RemoveNodeAtParticularPosition(1);
+            list6.Display();
+            Console.ReadKey();
+            //Create a sorted linked list in ascending order
+            Console.WriteLine("create a sorted linked list in acsending order");
+            LinkedList1 list7 = new LinkedList1();
+            list7.Add(56);
+            list7.Add(30);
+            list7.Add(40);
+            list7.Add(70);
+            Console.WriteLine("Before Sorting ");
+            list7.Display();
+            Console.WriteLine("After Sorting");
+            list7.sortList();
+            list7.Display();
             Console.ReadLine();
         }
 
