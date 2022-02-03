472fda8cf3be9b2af408f2aa6aa6d4302886d29c
Dipak@1432 <chavandipakr17@gmail.com>
2022-02-03 19:17:48 +0530

    Create simple linked list

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -11,6 +11,13 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked List");
+
+            LinkedList1 linkedlist = new LinkedList1();
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+            linkedlist.Display();
+
             Console.ReadLine();
         }
     }
